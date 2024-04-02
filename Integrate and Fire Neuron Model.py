1cefe26cf5df5324ae9d03537306645ebb304bcc
Manu Raghavan P V <manuraghavanofficial@gmail.com>
2024-04-02 12:13:05 +0100

    Update Integrate and Fire Neuron Model.py

--- Integrate and Fire Neuron Model.py
+++ Integrate and Fire Neuron Model.py
@@ -15,7 +15,7 @@ class IntegrateAndFireNeuron:
         delta_voltage = (input_current / self.membrane_resistance - self.membrane_voltage) / self.membrane_time_constant * time_step
         self.membrane_voltage += delta_voltage
 
-        # Check if threshold is crossed
+        # Checking if threshold is crossed
         if self.membrane_voltage >= self.threshold:
             self.membrane_voltage = self.reset_voltage  # Reset membrane voltage
             self.spike_times.append(time_step)  # Record spike time
@@ -34,23 +34,23 @@ class IntegrateAndFireNeuron:
         plt.legend()
         plt.show()
 
-# Simulation parameters
+# Simulation Parameters
 total_time = 100  # Total simulation time in milliseconds
 time_step = 0.1  # Time step size in milliseconds
 num_steps = int(total_time / time_step)  # Number of time steps
 
-# Input current (example: step input)
+# Step Input (Input Current)
 input_currents = np.zeros(num_steps)
 input_currents[int(num_steps/4):int(num_steps*3/4)] = 0.5  # Injecting current for a portion of time
 
 # Time steps
 time_steps = np.linspace(0, total_time, num_steps)
 
-# Create neuron object
+# Neuron object
 neuron = IntegrateAndFireNeuron(threshold=0.8, reset_voltage=0.0, membrane_resistance=1.0, membrane_time_constant=10.0)
 
-# Simulate neuron
+# Neuron Simulation
 neuron.simulate(input_currents, time_steps)
 
-# Plot membrane potential over time
+# Membrane potential over time
 neuron.plot_voltage(time_steps)
